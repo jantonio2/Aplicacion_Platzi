9ebe0f7717a29edcec7721cc437a67ee0626cddc
jantonio2 <jesusrochavedia@gmail.com>
2020-05-20 19:17:04 -0400

    Añadí un botón Favoritos en las imagenes del card

--- lib/card_image.dart
+++ lib/card_image.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'floating_action_button_green.dart';
 
 class CardImage extends StatelessWidget{
 
@@ -36,7 +37,13 @@ class CardImage extends StatelessWidget{
       ),
     );
 
-    return card;
+    return Stack(
+      alignment: Alignment(0.9,1.1),
+      children: <Widget>[
+        card,
+        FloatingActionButtonGreen()
+      ],
+    );
   }
 
 }
